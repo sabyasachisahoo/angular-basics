dcf137116b35efabc850de386828ebc5e72a5551
sabyasachisahoo <sabyasachisahoo77@gmail.com>
2019-03-02 13:48:09 +0530

    @ViewChild(by #reference) , setInterval() & clearInterval()

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import { GreetComponent } from './child/greet/greet.component';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +8,32 @@ import { GreetComponent } from './child/greet/greet.component';
 export class HomeComponent implements AfterViewInit {
   public message = 'Message from parent component by property[] binding';
   public childMessage = '';
-  @ViewChild(GreetComponent) greetFromChild: GreetComponent;
-  constructor() { }
+  public exitInterval;
 
+  /** here we are getting the reference directly from the html of own component using viewChild */
+  @ViewChild('templeteRefViewChild') inputValue: ElementRef;
+
+  constructor() {
+    /**here the inputValue for ViewChild willbe undifined,as the child is not render yet */
+    console.log(this.inputValue);
+
+    /** We can handle this undefined value, by using fat arrow function */
+    this.exitInterval = setInterval( () => {
+      this.postData();
+    }, 3000);
+  }
+
+  postData() {
+    const inputFromView = this.inputValue.nativeElement;
+    console.log(inputFromView.value);
+
+    if (inputFromView.value === 'close') {
+      clearInterval(this.exitInterval);
+    }
+  }
+
+/** We need to use AfterViewInit life cycle hook, as angular component follows a life cycle hooks */
   ngAfterViewInit() {
-    console.log(this.greetFromChild.greetParent());
+    this.inputValue.nativeElement.value = 'value is set using @ViewChild';
   }
 }
